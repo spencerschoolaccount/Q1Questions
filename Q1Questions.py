73bdcb7b4958356ec4c9ab6b5eae7cd198d0da1e
Spencer Hayes <113189856+spencerschoolaccount@users.noreply.github.com>
2022-12-06 14:50:50 +0000

    Added new questions and fixed a bug in Q1Questions

--- Q1Questions.py
+++ Q1Questions.py
@@ -3,7 +3,7 @@ answerA = input('\nWhat is Answer A?\n') + '\n'
 answerB = input('\nWhat is Answer B?\n') + '\n'
 answerC = input('\nWhat is Answer C?\n') + '\n'
 answerD = input('\nWhat is Answer D?\n') + '\n'
-correctAnswer = input('\nWhich is the correct answer? (A, B, C, D)\n')
+correctAnswer = input('\nWhich is the correct answer? (A, B, C, D)\n').upper()
 if correctAnswer != 'A' and correctAnswer != 'B' and correctAnswer != 'C' and correctAnswer != 'D':
   exit("Invalid correct answer, must be A, B, C, or D")
 
